ec682fe24d1059343eb3507c45002105115d646c
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-11 00:15:22 -0700

    Use `setfenv` for globals

--- src/globals/rostruct-globals.ts
+++ src/globals/rostruct-globals.ts
@@ -10,15 +10,11 @@ import { VirtualEnv } from "core";
 interface Globals {
 	/** A number used to identify Reconciler objects. */
 	currentScope: number;
-
-	/** List of environments for running VirtualScripts. */
-	environments: Map<string, VirtualEnv>;
 }
 
 /** Global environment reserved for Rostruct. */
 export const globals: Globals = (getgenv().Rostruct as Globals) || {
 	currentScope: 0,
-	environments: new Map<string, VirtualEnv>(),
 };
 
 getgenv().Rostruct = globals;
