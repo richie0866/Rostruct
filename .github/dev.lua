7928959f12adcd048a0e9af42b420312556f1b20
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-19 18:23:57 -0700

    Update dev.lua

--- .github/dev.lua
+++ .github/dev.lua
@@ -1,10 +1,33 @@
 
 local Rostruct = TS.initialize("init")
-local rostruct = Rostruct.build("tests/build/")
 
-if (game.Lighting:FindFirstChild("build")) then
-	game.Lighting.build:Destroy()
-end
-rostruct.tree.Parent = game.Lighting
+local build
+build = Rostruct.build("Util/MidiPlayer/src/")
+build = Rostruct.fetch("src/", "Roblox", "roact", "v1.4.0"):expect()
+build = Rostruct.fetchLatest("src/", "Roblox", "roact"):expect()
 
-rostruct:defer()
+table.foreach(build.fetchInfo, print)
+
+local roact = build.tree
+roact.Name = "Roact"
+
+-- Simulate localscript runtime
+local scriptsPromise = build:defer()
+print(scriptsPromise)
+
+local Roact
+
+-- Require top-level instance
+Roact = build:require():expect()
+print(Roact.createElement)
+
+-- Require top-level instance (async)
+Roact = build:requireAsync()
+print(Roact.createElement)
+
+-- Require Roact (inline)
+Roact = Rostruct.fetchLatest("src/", "Roblox", "roact")
+	:expect()
+    :set({ Name = "Roact" })
+    :requireAsync()
+print(Roact.createElement)
