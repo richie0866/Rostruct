5a6c26309fd4b7cebf481e27bb638a4c4502fea7
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-19 18:28:03 -0700

    Update extract.ts

--- src/utils/extract.ts
+++ src/utils/extract.ts
@@ -6,9 +6,8 @@ import { FileArray, makeFiles, pathUtils } from "utils/file-utils";
  * @param rawData Raw zip data.
  * @param target The directory to extract files to.
  * @param ungroup
- * If the zip file contains a single directory with everthing in it, it may be useful to
- * extract data excluding the folder. This parameter controls whether the top directory
- * is ignored when extracting.
+ * If the zip file contains a single directory, it may be useful to ungroup the files inside.
+ * This parameter controls whether the top-level directory is ignored.
  */
 export function extract(rawData: string, target: string, ungroup?: boolean) {
 	const zipData = zzlib.unzip(rawData);
@@ -16,13 +15,11 @@ export function extract(rawData: string, target: string, ungroup?: boolean) {
 
 	// Convert the path-content map to a file array
 	for (const [path, contents] of zipData)
-		if (ungroup) {
-			// Trim the first folder off the path if 'ungroup' is true
-			fileArray.push([pathUtils.addTrailingSlash(target) + (path.gsub("^([^/]*/)", "")[0] as string), contents]);
-		} else {
-			fileArray.push([pathUtils.addTrailingSlash(target) + path, contents]);
-		}
+		ungroup
+			? // Trim the first folder off the path if 'ungroup' is true
+			  fileArray.push([pathUtils.addTrailingSlash(target) + path.match("^[^/]*/(.*)$")[0], contents])
+			: fileArray.push([pathUtils.addTrailingSlash(target) + path, contents]);
 
-	// Make the files at the given target.
+	// Make the files at the given target
 	makeFiles(fileArray);
 }
