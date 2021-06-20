1a87bf8a87e9c0069100094db730ab83df59b27c
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-19 18:24:58 -0700

    Use const functions

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,22 +1,21 @@
 import { makeFiles } from "utils/file-utils";
 
 /** Assigns common folders to a keyword. */
-const FILES = {
+const Shortcut = {
 	ROOT: "rostruct/",
 	CACHE: "rostruct/cache/",
 	RELEASE_CACHE: "rostruct/cache/releases/",
 	RELEASE_TAGS: "rostruct/cache/release_tags.json",
 } as const;
 
+type Shortcut = typeof Shortcut;
+
 /** Gets a Rostruct path from a keyword. */
-export function getRostructPath<T extends keyof typeof FILES>(keyword: T): typeof FILES[T] {
-	return FILES[keyword];
-}
+export const getRostructPath = <T extends keyof Shortcut>(keyword: T): Shortcut[T] => Shortcut[keyword];
 
 /** Sets up core files for Rostruct. */
-export function bootstrap() {
+export const bootstrap = () =>
 	makeFiles([
 		["rostruct/cache/releases/", ""],
 		["rostruct/cache/release_tags.json", "{}"],
 	]);
-}
