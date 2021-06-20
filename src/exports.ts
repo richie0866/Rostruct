939c33bd2885e99449b6cc383d5339a84fafe82c
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-19 18:27:54 -0700

    Use `exports.ts`

---
+++ src/exports.ts
@@ -0,0 +1,5 @@
+import _Promise from "modules/Promise";
+
+export const Promise: PromiseConstructor = _Promise;
+
+export { clearReleaseCache } from "utils/fetch-github-release";
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 /**
- * @description Rostruct builds your Lua projects from the filesystem.
+ * Rostruct
+ * @description Build your Lua projects from the filesystem.
  * @author 0866
  */
 
 import { bootstrap } from "bootstrap";
-import Promise from "modules/Promise";
 
 bootstrap();
 
-export { Promise };
-
-export { build, fetch, fetchLatest, fetchAndBuild, fetchLatestAndBuild } from "Rostruct";
+export * from "Rostruct";
+export * from "exports";
