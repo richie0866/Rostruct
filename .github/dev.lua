205940e2cdbf7945fac1ea281701da0fec23f4dc
Richard <74037129+richie0866@users.noreply.github.com>
2021-06-21 18:23:21 -0700

    Update dev.lua

--- .github/dev.lua
+++ .github/dev.lua
@@ -1,33 +1,83 @@
 
 local Rostruct = TS.initialize("init")
 
-local build
-build = Rostruct.build("Util/MidiPlayer/src/")
-build = Rostruct.fetch("src/", "Roblox", "roact", "v1.4.0"):expect()
-build = Rostruct.fetchLatest("src/", "Roblox", "roact"):expect()
+local function forEach(t, f)
+	for i, v in pairs(t) do
+		f(i, v)
+	end
+end
 
-table.foreach(build.fetchInfo, print)
+warn("\n--- Fetch GitHub release ---")
+do
+	local package
+	package = Rostruct.fetchAsync("Roblox", "roact", "v1.4.0")
+	package = Rostruct.fetchLatestAsync("Roblox", "roact")
+	forEach(package.fetchInfo, print)
+end
 
-local roact = build.tree
-roact.Name = "Roact"
+warn("\n--- Test MidiPlayer ---")
+do
+	local package = Rostruct.new("Util/MidiPlayer/")
 
--- Simulate localscript runtime
-local scriptsPromise = build:defer()
-print(scriptsPromise)
+	--[[
+		Build project using start
+	]]
+	package:build("src/", { Name = "MidiPlayer" })
+	print(package:start())
 
-local Roact
+	--[[
+		Require inner module
+	]]
+	warn("--- MIDI ---")
+	forEach(package:requireAsync(package.tree.MidiPlayer.MIDI), print)
+	warn("--- End of MIDI ---")
 
--- Require top-level instance
-Roact = build:require():expect()
-print(Roact.createElement)
+	--[[
+		Require specific file
+	]]
+	warn("--- Thread ---")
+	package:build("src/Util/Thread.lua")
+	forEach(package:requireAsync(package.tree.Thread), print)
+	warn("--- End of Thread ---")
+end
 
--- Require top-level instance (async)
-Roact = build:requireAsync()
-print(Roact.createElement)
+warn("\n--- Try all file types ---")
+do
+	local package = Rostruct.new("tests/build/")
+	package:build()
+	package:start():await()
+end
 
--- Require Roact (inline)
-Roact = Rostruct.fetchLatest("src/", "Roblox", "roact")
-	:expect()
-    :set({ Name = "Roact" })
-    :requireAsync()
-print(Roact.createElement)
+warn("\n--- Require Roact ---")
+do
+	local package = Rostruct.fetchAsync("Roblox", "roact", "v1.4.0")
+	
+	local Roact = package:requireAsync(
+		package:build("src/", { Name = "Roact" })
+	)
+
+	print(Roact.createElement)
+end
+
+warn("\n--- Require Roact (inline) ---")
+do
+	local Roact = Rostruct.fetchLatest("Roblox", "roact")
+		:andThen(function(rostruct)
+			return rostruct:require(rostruct:build("src/", { Name = "Roact" }))
+		end)
+		:expect()
+
+	print(Roact.createElement)
+end
+
+warn("\n--- Require Roact (inline + tree) ---")
+do
+	local Roact = Rostruct.fetchLatest("Roblox", "roact")
+		:andThen(function(rostruct)
+			rostruct:build("src/", { Name = "Roact" })
+			return rostruct:require(rostruct.tree.Roact)
+		end)
+		:expect()
+
+	print(Roact.createElement)
+end
